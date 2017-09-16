71b66330fc445b8a77f1f492f4da9798769ca6a6
funwithcthulhu <thomasboeding@gmail.com>
2017-09-16 09:21:28 -0500

    updated valid_triangle to return area

--- randcode/valid_triangle.c
+++ randcode/valid_triangle.c
@@ -3,8 +3,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int valid_triangle(float a, float b, float c);
+float display_area(float a, float b, float c);
 
 int main(int argc, char *argv[])
 {
@@ -13,6 +15,7 @@ int main(int argc, char *argv[])
 	float b;
 	float c;
 
+	// if 3 arguments given, assigns them to a, b, c and converts them to floats
 	if (argc == 4) {
 
 		a = atof(argv[1]); 
@@ -20,11 +23,16 @@ int main(int argc, char *argv[])
 		c = atof(argv[3]);
 		printf("%f, %f, %f\n", a, b, c);
 	} else {
+		// if 3 args not given, returns an error and exits
 		printf("Input requires three sides\n");
 		exit(1);
 	}
+	// If the three sides make a valid triangle, prints 'true' and the triangle's area
 	if (valid_triangle(a, b, c)) {
+		float area = display_area(a, b, c);
 		printf("true\n");
+		printf("Area of this triangle = %.5f\n", area);
+
 	} else {
 		printf("false\n");
 	}
@@ -33,11 +41,24 @@ int main(int argc, char *argv[])
 
 int valid_triangle(float a, float b, float c)
 {
+	// checks triangle sides are greater than 0
 	if (a <= 0 || b <= 0 || c <= 0) {
 		return 0;
 	}
+	// checks that each side is longer than the addition of the other sides
+	// this is a prerequisite for being a triangle
 	if (a + b <= c || a + c <= b || b + c <= a) {
 		return 0;
 	}
 	return 1;
 }
+
+float display_area(float a, float b, float c)
+{
+	// Uses Heron's formula to determine area of the triangle
+	float s = (a + b + c) / 2; // find semi-perimeter of triangle
+	float area = sqrt(s * (s - a) * (s - b) * (s - c));
+	return area;
+}
+
+
