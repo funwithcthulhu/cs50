1222b22e44f4c9034669f21d1031a0b432088ccd
funwithcthulhu <thomasboeding@gmail.com>
2017-09-15 19:44:05 -0500

    added documentation for initials

--- pset2/initials.c
+++ pset2/initials.c
@@ -4,13 +4,17 @@
 
 int main(void)
 {
+	// get input from user
 	char *name; // or char name[100] 
 	fgets (name, 100, stdin);
 
+	// check first character for letter or space
 	if (name[0] != ' ')
 	{
 		printf("%c", toupper(name[0]));
 	}
+	// loop through length of string, printing letters
+	// with a space in front of them (initials)
 	for (int i = 0, l = strlen(name); i < l; i++)
 	{
 		if (name[i] != ' ' && name[i - 1] == ' ')
